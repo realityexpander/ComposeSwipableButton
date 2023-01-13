4f85afc932d1b42c1be4480f41b7f0520bd526f8
Chris Athanas <realityexpander@gmail.com>
2023-01-13 14:52:58 -0600

    Initial commit

---
+++ app/src/main/java/com/realityexpander/composeswipablebutton/MainActivity.kt
@@ -0,0 +1,226 @@
+package com.realityexpander.composeswipablebutton
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Done
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.realityexpander.composeswipablebutton.ui.theme.ComposeSwipableButtonTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeSwipableButtonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        SwipeButtonSample()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SwipeButtonSample() {
+    val coroutineScope = rememberCoroutineScope()
+    val (isComplete, setIsComplete) = remember {
+        mutableStateOf(false)
+    }
+
+    SwipeButton(
+        text = "SAVE",
+        isComplete = isComplete,
+        onSwipe = {
+            coroutineScope.launch {
+                delay(2000)
+                setIsComplete(true)
+            }
+        },
+    )
+}
+
+@Composable
+fun SwipeIndicator(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(2.dp)
+            .clip(CircleShape)
+            .aspectRatio(
+                ratio = 1.0F,
+                matchHeightConstraintsFirst = true,
+            )
+            .background(Color.White),
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Lock,
+            contentDescription = null,
+            tint = backgroundColor,
+            modifier = Modifier.size(36.dp),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeButton(
+    text: String,
+    isComplete: Boolean,
+    doneImageVector: ImageVector = Icons.Rounded.Done,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color(0xFF03A9F4),
+    onSwipe: () -> Unit,
+) {
+    val width = 200.dp
+    val widthInPx = with(LocalDensity.current) {
+        width.toPx()
+    }
+    val anchors = mapOf(
+        0F to 0,
+        widthInPx to 1,
+    )
+    val swipeableState = rememberSwipeableState(0)
+    val (swipeComplete, setSwipeComplete) = remember {
+        mutableStateOf(false)
+    }
+    val alpha: Float by animateFloatAsState(
+        targetValue = if (swipeComplete) {
+            0F
+        } else {
+            1F
+        },
+        animationSpec = tween(
+            durationMillis = 300,
+            easing = LinearEasing,
+        )
+    )
+
+    LaunchedEffect(
+        key1 = swipeableState.currentValue,
+    ) {
+        if (swipeableState.currentValue == 1) {
+            setSwipeComplete(true)
+            onSwipe()
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .padding(
+                horizontal = 48.dp,
+                vertical = 16.dp,
+            )
+            .clip(CircleShape)
+            .background(backgroundColor)
+            .animateContentSize()
+            .then(
+                if (swipeComplete) {
+                    Modifier.width(64.dp)
+                } else {
+                    Modifier.fillMaxWidth()
+                }
+            )
+            .requiredHeight(64.dp),
+    ) {
+        SwipeIndicator(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .alpha(alpha)
+                .offset {
+                    IntOffset(swipeableState.offset.value.roundToInt(), 0)
+                }
+                .swipeable(
+                    state = swipeableState,
+                    anchors = anchors,
+                    thresholds = { _, _ ->
+                        FractionalThreshold(0.8F)
+                    },
+                    orientation = Orientation.Horizontal,
+                )
+                .rotate(180F * swipeableState.offset.value / widthInPx),
+            backgroundColor = backgroundColor,
+        )
+        Text(
+            text = text,
+            color = White,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .alpha(alpha)
+                .padding(
+                    horizontal = 80.dp,
+                )
+                .offset {
+                    IntOffset(swipeableState.offset.value.roundToInt(), 0)
+                },
+        )
+        AnimatedVisibility(
+            visible = swipeComplete && !isComplete,
+        ) {
+            CircularProgressIndicator(
+                color = White,
+                strokeWidth = 1.dp,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(4.dp),
+            )
+        }
+        AnimatedVisibility(
+            visible = isComplete,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            Icon(
+                imageVector = doneImageVector,
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(44.dp),
+            )
+        }
+    }
+}
